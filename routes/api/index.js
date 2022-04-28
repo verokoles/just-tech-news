ad80a89728f60b2378176f6b93e41e1c107409f6
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-04-28 15:02:09 -0500

    add delte and post routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
-
+router.use('/posts', postRoutes);
 
 
 
